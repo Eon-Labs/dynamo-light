3e980d37166307e7408bf5eab522ee96c7c73894
Chen Li <chenli@tableau.com>
2019-08-09 12:18:41 -0700

    fix: use a unique name for the .env file to avoid conflicting with existing projects

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,8 +2,8 @@ import { config } from "dotenv";
 import { existsSync } from "fs";
 import { resolve } from "path";
 
-const envProjectPath = "./.env";
-const envRelativePath = "../../.env";
+const envProjectPath = "./.env.dynamo.light";
+const envRelativePath = "../../.env.dynamo.light";
 
 if (existsSync(envProjectPath)) {
   config({ path: resolve(__dirname, envRelativePath) });
